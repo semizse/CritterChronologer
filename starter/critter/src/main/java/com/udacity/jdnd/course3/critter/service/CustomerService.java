e2702fd4ee51a75cef1b493d819f3019b2fb0d17
Serdar Semiz <smzserdar@gmail.com>
2020-07-16 18:55:46 +0300

    Transactional annotation added

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -4,14 +4,17 @@ import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.repository.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repository.PetRepository;
+import com.udacity.jdnd.course3.critter.utility.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class CustomerService {
 
     @Autowired
